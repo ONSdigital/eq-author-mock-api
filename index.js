4ab880aff4b62637fec95d6f5de6f255af3b402c
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-07-18 15:08:54 +0100

    Add function to mock the GraphQL schema.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-/**
- * Created by samiwel on 18/07/2017.
- */
+const buildClientSchema = require('graphql').buildClientSchema;
+const addMockFunctionsToSchema = require('graphql-tools').addMockFunctionsToSchema;
+const merge = require('lodash').merge;
+
+const graphqlSchema = require('eq-author-graphql-schema/author.graphqls');
+
+export function mockSchema(mocks, preserveResolvers=false) {
+    const schema = buildClientSchema(graphqlSchema);
+    addMockFunctionsToSchema(merge({}, {mocks}, {preserveResolvers}));
+    return schema;
+};
